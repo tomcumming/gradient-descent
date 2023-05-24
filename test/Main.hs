b61aac8e87b4cc5e1e108a5a10553c67916de562
Tom Cumming <mail@tomcumming.co.uk>
2023-05-25 00:22:06 +0100

    Slightly harder test

--- test/Main.hs
+++ test/Main.hs
@@ -4,35 +4,39 @@ import Control.Monad (replicateM)
 import Data.Maybe (listToMaybe)
 import Data.Sized (Sized)
 import Data.Sized qualified as Sized
+import Data.Type.Ordinal (ordToNatural)
 import Data.Vector (Vector)
+import GHC.TypeLits (KnownNat)
 import Math.GradientDescent (Config (..), ErrorFunc, Solution (..), gradientDescent)
 import Test.Falsify.Generator qualified as Gen
 import Test.Falsify.Range qualified as Range
 import Test.Tasty (defaultMain)
 import Test.Tasty.Falsify (Property, gen, testProperty)
 
-easyConcave :: Sized Vector n Double -> ErrorFunc n
-easyConcave ss xs =
-  sum $
-    Sized.zipWith
-      (\x s -> x * x * realToFrac s)
-      xs
-      ss
+easyConcave :: KnownNat n => Sized Vector n Double -> ErrorFunc n
+easyConcave origin =
+  sum
+    . Sized.zipWithSame
+      (\s x -> s * x * x)
+      (Sized.generate' $ \idx -> 0.1 * fromIntegral (1 + ordToNatural idx))
+    . Sized.zipWithSame
+      (\o x -> x - realToFrac o)
+      origin
 
 easyConcaveProp :: Property ()
 easyConcaveProp = do
   let cfg = Config {cfgInitialStepSize = 1, cfgGrow = 2, cfgShrink = 0.5}
-  let scales :: Sized Vector 3 Double = Sized.unsafeFromList' [0.1, 0.2, 0.3]
+  let zero :: Sized Vector 3 Double = Sized.replicate' 0
   [x, y, z] <-
     replicateM 3 $
       realToFrac <$> gen (Gen.integral $ Range.withOrigin (-10, 10) (0 :: Int))
-  let initial :: Sized Vector 3 Double = Sized.unsafeFromList' [x, y, z]
-  let steps = take 10 $ gradientDescent cfg (easyConcave scales) initial
+  let origin :: Sized Vector 3 Double = Sized.unsafeFromList' [x, y, z]
+  let steps = take 10 $ gradientDescent cfg (easyConcave origin) zero
   let solutions = concatMap (either (const []) pure) steps
   case listToMaybe (reverse solutions) of
     Nothing -> fail "No solutions found"
     Just Solution {..}
-      | solError < 0.1 -> pure ()
+      | solError < 0.01 -> pure ()
       | otherwise -> fail $ "Best solution was poor: " <> show solError
 
 main :: IO ()
